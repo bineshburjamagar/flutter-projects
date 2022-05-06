30bbc78e526ba36d7c1131927a3555c31e96d71c
Binesh Burja Magar <bineshburzaa@gmail.com>
2022-05-06 23:09:09 +0545

    mi card final commit

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -11,73 +11,73 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 159, 124, 255),
+        backgroundColor: const Color.fromARGB(255, 105, 52, 249),
         body: SafeArea(
-          child: Column(children: [
-            const CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/vedagrind.png'),
-            ),
-            const Text(
-              'Veda Grind',
-              style: TextStyle(
-                fontSize: 50.0,
-                color: Colors.white,
-                fontFamily: 'Caveat',
-              ),
-            ),
-            const Text(
-              'UI UX DEVELOPER',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 186, 224, 255),
-                fontFamily: 'BebasNeue',
-                letterSpacing: 3.5,
-              ),
-            ),
-            Container(
-              color: const Color.fromARGB(255, 186, 224, 255),
-              padding: const EdgeInsets.all(10.0),
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-              child: Row(
-                children: const [
-                  Icon(
-                    Icons.phone,
-                    color: Colors.black,
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/vedagrind.png'),
+                ),
+                Text(
+                  'Veda Grind',
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    color: Colors.white,
+                    fontFamily: 'Caveat',
                   ),
-                  SizedBox(width: 10.0),
-                  Text(
-                    '+97798012345678',
-                    style: TextStyle(fontSize: 20.0, color: Colors.black),
-                  )
-                ],
-              ),
-            ),
-            Container(
-              color: const Color.fromARGB(255, 186, 224, 255),
-              padding: const EdgeInsets.all(10.0),
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-              child: Row(
-                children: const [
-                  Icon(
-                    Icons.email,
-                    color: Colors.black,
+                ),
+                Text(
+                  'UI UX DEVELOPER',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 186, 224, 255),
+                    fontFamily: 'BebasNeue',
+                    letterSpacing: 3.5,
                   ),
-                  SizedBox(width: 10.0),
-                  Text(
-                    'vedagrind@gmail.com',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.black,
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal,
+                  ),
+                ),
+                Card(
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Color.fromARGB(255, 105, 52, 249),
+                      ),
+                      title: Text(
+                        '+97798012345678',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            color: Color.fromARGB(255, 105, 52, 249)),
+                      ),
+                    )),
+                Card(
+                  margin:
+                      EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Color.fromARGB(255, 105, 52, 249),
                     ),
-                  )
-                ],
-              ),
-            )
-          ]),
+                    title: Text(
+                      'vedagrind@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Color.fromARGB(255, 105, 52, 249),
+                      ),
+                    ),
+                  ),
+                )
+              ]),
         ),
       ),
     );
