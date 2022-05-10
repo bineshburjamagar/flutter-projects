bd1f976e889a4ba1d8f9d29be6ed37f38c759816
Binesh Burja Magar <bineshburzaa@gmail.com>
2022-05-11 00:07:52 +0545

    adding functions

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const XylophoneApp());
@@ -6,13 +9,43 @@ void main() {
 
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({Key? key}) : super(key: key);
+  void playSound(int soundNum) {
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
+  Expanded buildkey() {
+    return Expanded(
+      child: FlatButton(
+        color: Colors.amberAccent,
+        onPressed: () {
+          playSound(1);
+        },
+        child: const Text(''),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Text('gg'),
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                buildkey(),
+                buildkey(),
+                buildkey(),
+                buildkey(),
+                buildkey(),
+                buildkey(),
+                buildkey(),
+              ],
+            ),
+          ),
         ),
       ),
     );
