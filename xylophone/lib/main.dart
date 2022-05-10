14628e7f5ab49c001ae0de95e28d306c024435bb
Binesh Burja Magar <bineshburzaa@gmail.com>
2022-05-10 20:04:00 +0545

    initial commit

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Text('gg'),
+        ),
+      ),
+    );
+  }
+}
