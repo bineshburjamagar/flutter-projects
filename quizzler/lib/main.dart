572c2fbc02b6ee69386d22d0699781eb7d4a016e
Binesh Burja Magar <bineshburzaa@gmail.com>
2022-05-12 23:29:53 +0545

    adding questions and checking answers using if else

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: deprecated_member_use, unnecessary_new
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const Quizzler());
@@ -11,7 +13,7 @@ class Quizzler extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Color.fromARGB(255, 27, 46, 56),
         body: SafeArea(
           child: Padding(
               padding: EdgeInsets.symmetric(horizontal: 10.0),
@@ -30,21 +32,31 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  List<bool> answers = [false, true, true];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        const Expanded(
+      children: <Widget>[
+        Expanded(
           flex: 5,
           child: Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'Heres is questions',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 25.0, color: Colors.white60),
+                style: const TextStyle(fontSize: 25.0, color: Colors.white60),
               ),
             ),
           ),
@@ -65,6 +77,17 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswers = answers[questionNumber];
+                if (correctAnswers == true) {
+                  // ignore: avoid_print
+                  print('correct');
+                } else {
+                  // ignore: avoid_print
+                  print('incorrect');
+                }
+                setState(() {
+                  questionNumber++;
+                });
                 //gg
               },
             ),
@@ -86,10 +109,28 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //gg
+                bool correctAnswers = answers[questionNumber];
+                if (correctAnswers == false) {
+                  // ignore: avoid_print
+                  print('correct');
+                } else {
+                  // ignore: avoid_print
+                  print('incorrect');
+                }
+                setState(() {
+                  // scoreKeeper.add(const Icon(
+                  //   Icons.close_outlined,
+                  //   color: Colors.red,
+                  // ));
+
+                  questionNumber++;
+                });
               },
             ),
           ),
+        ),
+        Row(
+          children: scoreKeeper,
         )
       ],
     );
