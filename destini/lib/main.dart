ea4c8a348dbdf80c72a26c5a18a433867fd3a3f6
Binesh Burja Magar <bineshburzaa@gmail.com>
2022-05-16 19:03:41 +0545

    initial commit

---
+++ destini/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Destini());
+
+class Destini extends StatelessWidget {
+  const Destini({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+    );
+  }
+}
